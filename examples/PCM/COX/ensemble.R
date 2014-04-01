99edd7854eb26b00e13a72117bb232cd3efcc9be
Isidro <isidrolauscher@gmail.com>
2014-04-01 17:44:37 +0200

    Knitter modified, final version, added.

--- examples/PCM/COX/ensemble.R
+++ examples/PCM/COX/ensemble.R
@@ -1,31 +1,17 @@
-#!/c5/shared/R/3.0.2/bin/Rscript
+library(devtools) || stop("package devtools need to be installed")
+library(pbapply) || stop("package pbapply need to be installed")
 
 
-#install.packages("devtools")
-library(devtools)
-library(caret)
-library(pbapply)
-#install_github('caretEnsemble', 'zachmayer')
-#install.packages("/Bis/home/icortes/Desktop/PCM/DHFR/ensemble/caretEmsemble",repos=NULL)
-library(caretEnsemble)
-library(gbm)
-source("setup.R")
-
-
-#dataa < readRDS("dataa.rds")
-typeof(dataa)
+source("setup_ensemble.R")
 
 all.models <- list()
 
-models <- as.vector(read.table("models_ensemble_best")$V1)
+models <- as.vector(read.table("models_ensemble")$V1)
 
 for (i in 1:length(models)){
-
 model_load = paste("readRDS('",models[i],"')",sep="")
 assign(paste("model_",i,sep=""), eval(parse(text=model_load)))
-
 all.models[[length(all.models)+1]] <- eval(parse(text=paste("model_",i,sep="")))
-
 }
 
 names(all.models) <- sapply(all.models, function(x) x$method)
@@ -36,38 +22,39 @@ greedy <- caretEnsemble(all.models, iter=1000L)
 sort(greedy$weights, decreasing=TRUE)
 
 # make a linear regression ensemble
-linear <- caretStack(all.models, method='glm')#, trControl=dataa$trainControl(method='cv'))
+linear <- caretStack(all.models, method='glm')
 summary(linear$ens_model$finalModel)
-linear$error
 
-# make a rf regression ensemble
-#tune.grid <- expand.grid(.mtry = seq(1,length(all.models),1))
-#nonlinear <- caretStack(all.models, method='rf', trControl=trainControl(method='cv'), tune.grid=tune.grid)
-#summary(nonlinear$ens_model$finalModel)
-#nonlinear$error
+# make Elastic Net ensemble
+enet_ens <- caretStack(all.models, method='enet')
+coefs_enet_ens <- enet_ens$ens_model$finalModel$beta.pure[ncol(enet_ens$ens_model$finalModel$beta.pure)+1,]
 
-#Predict for test set:
+# make SVM linear ensemble
+trControl <- trainControl(method = "cv",  number=5)
+tune.grid <- expand.grid(.C=expGrid(power.from=-14,power.to=10,power.by=1,base=2))
+linear_svm <- caretStack(all.models, method='svmLinear',trControl=trControl,tuneGrid=tune.grid)
+
+# make SVM radial ensemble
+trControl <- trainControl(method = "cv",  number=5)
+tune.grid <- expand.grid(.sigma=expGrid(power.from=-14,power.to=10,power.by=1,base=2),
+                         .C=expGrid(power.from=-14,power.to=10,power.by=2,base=2))
+radial_svm <- caretStack(all.models, method='svmRadial',trControl=trControl,tuneGrid=tune.grid)
+
+
+# Predict the bioactivities for test set:
 preds <- data.frame(sapply(all.models, predict, newdata=dataa$x.test))
 preds$ENS_greedy <- predict(greedy, newdata=dataa$x.test)
 preds$ENS_linear <- predict(linear, newdata=dataa$x.test)
-#preds$ENS_nonlinear <- predict(nonlinear, newdata=x.test)
-sort(sqrt(colMeans((preds - y.test) ^ 2)))
+preds$ENS_enet <- predict(enet_ens, newdata=x.test)
+preds$ENS_SVMrad <- predict(radial_svm, newdata=x.test)c
+preds$ENS_SVMlin <- predict(linear_svm, newdata=x.test)
 
-# Calculate metrics
-source("../functions.R")
-print("Q2")
+
+# Calculate metrics (We could also apply Validation instead.)
 Q2s = apply(preds,2, function(x) Qsquared(x,dataa$y.test))
-print("R2")
 R2s <- apply(preds,2, function(x) Rsquared(x,dataa$y.test))
-print("R20")
 R20s <- apply(preds,2, function(x) Rsquared0(x,dataa$y.test))
-print("RMSE")
 RMSEs <- apply(preds,2, function(x) RMSE(x,dataa$y.test))
 
-RMSEs
-Q2s
-
-save(greedy, file="greedy_best.RData")
-save(linear, file="linear_best.RData")
-
-save(list=ls(), file="ensemble_COX_best.RData")
+# We sort the models by RMSE
+sort(RMSEs)
