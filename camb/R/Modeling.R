7ef138da721b2027f36858cdf2b27d95b5591634
Isidro <isidrolauscher@gmail.com>
2014-05-06 15:23:23 +0200

    Differnt Qsquared formulas added // Tutorial considerably extended

--- camb/R/Modeling.R
+++ camb/R/Modeling.R
@@ -26,9 +26,9 @@ RemoveColumnsWithMoreThanHalfNA <- function(d) {
 }
                          
 ##############
-ImputeFeatures <- function(d, k=10) {
+ImputeFeatures <- function(d, k=10,...) {
   suppressWarnings(require(impute)) || stop("Package impute is required. Install from CRAN or Bioconductor -depending on the R version you are using-.")
-  as.data.frame(impute.knn(as.matrix(d), k = k)$data)
+  as.data.frame(impute.knn(as.matrix(d), k = k,...)$data)
 }
 
 ##############
@@ -53,8 +53,8 @@ SplitSet <- function(ids, x, y, percentage = 20, seed = 1) {
 }
 
 ##############
-RemoveNearZeroVarianceFeatures <- function(ss, frequencyCutoff = 30/1) {
-  nzv.columns <- nearZeroVar(ss$x.train, freqCut = frequencyCutoff)
+RemoveNearZeroVarianceFeatures <- function(ss, frequencyCutoff = 30/1,...) {
+  nzv.columns <- nearZeroVar(ss$x.train, freqCut = frequencyCutoff,...)
   if (length(nzv.columns) != 0) {
     message(paste(length(nzv.columns), "features removed with variance below cutoff"))
     ss$x.train <- ss$x.train[, -nzv.columns]
@@ -67,8 +67,8 @@ RemoveNearZeroVarianceFeatures <- function(ss, frequencyCutoff = 30/1) {
 }
 
 ##############
-RemoveHighlyCorrelatedFeatures <- function(ss, correlationCutoff = 0.95) {
-  hc.columns <- findCorrelation(cor(ss$x.train), correlationCutoff)
+RemoveHighlyCorrelatedFeatures <- function(ss, correlationCutoff = 0.95,...) {
+  hc.columns <- findCorrelation(cor(ss$x.train), correlationCutoff,...)
   if (length(hc.columns) != 0) {
     message(paste(length(hc.columns), "features removed with correlation above cutoff"))
     ss$x.train <- ss$x.train[, -hc.columns]
@@ -81,8 +81,8 @@ RemoveHighlyCorrelatedFeatures <- function(ss, correlationCutoff = 0.95) {
 }
 
 ##############
-PreProcess <- function(ss, steps = c("center", "scale")) {
-  transformation <- preProcess(ss$x.train, method = steps)
+PreProcess <- function(ss, steps = c("center", "scale"),...) {
+  transformation <- preProcess(ss$x.train, method = steps,...)
   ss$x.train <- predict(transformation, ss$x.train)
   ss$x.holdout <- predict(transformation, ss$x.holdout)
   ss$transformation <- transformation
@@ -90,12 +90,12 @@ PreProcess <- function(ss, steps = c("center", "scale")) {
 }
 
 ##############
-GetCVTrainControl <- function(ss, seed = 1, folds = 5, repeats = 1) {
+GetCVTrainControl <- function(ss, seed = 1, folds = 5, repeats = 1,method='cv',returnResamp='none',returnData=TRUE, savePredictions=TRUE,verboseIter=TRUE, allowParallel=TRUE,...) {
   set.seed(seed)
   ss$trControl <- trainControl(method='cv', number=folds, repeats=repeats, returnResamp='none',
                                returnData=TRUE, savePredictions=TRUE,
                                verboseIter=TRUE, allowParallel=TRUE,
-                               index=createMultiFolds(ss$y.train, k=folds, times=repeats))
+                               index=createMultiFolds(ss$y.train, k=folds, times=repeats),...)
   ss
 }
 
