a6149127122de2efa5699f2695a62395e595a21b
Daniel Murrell <dsmurrell@gmail.com>
2014-07-25 14:54:16 +0100

    adding main package documentation

--- camb/R/camb.R
+++ camb/R/camb.R
@@ -1,25 +1,10 @@
-#' Dates and times made easy with lubridate.
+#' Chemically Aware Model Builder (camb): An R package for property and bioactivity modeling of small molecules
 #'
-#' Lubridate provides tools that make it easier to parse and 
-#' manipulate dates. These tools are grouped below by common 
-#' purpose. More information about each function can be found in 
-#' its help documentation.
-#'
-#' Parsing dates
-#'
-#' Lubridate's parsing functions read strings into R as POSIXct 
-#' date-time objects. Users should choose the function whose name 
-#' models the order in which the year ('y'), month ('m') and day 
-#' ('d') elements appear the string to be parsed: 
-#' \code{\link{dmy}}, \code{\link{myd}}, \code{\link{ymd}}, 
-#' \code{\link{ydm}}, \code{\link{dym}}, \code{\link{mdy}}, 
-#' \code{\link{ymd_hms}}). 
+#' camb allows molecule standardisation, descriptor generation, model building, model ensembling and new molecule
+#' predictions to be done within the same environment.
 #' 
-#' ...
-#' 
-#' @references Garrett Grolemund, Hadley Wickham (2011). Dates and Times
-#'   Made Easy with lubridate. Journal of Statistical Software, 40(3),
-#'   1-25. \url{http://www.jstatsoft.org/v40/i03/}.
+#' @references Daniel Murrell, Isidro Cortes (2014). Chemically Aware Model Builder (camb): An R package for property and bioactivity modeling of small molecules. 
+#' Submitted to Bioinformatics.
 #' @docType package
 #' @name camb
 #' @import kernlab gbm rJava caret impute ggplot2 grid gridExtra protr vegan pbapply caTools
